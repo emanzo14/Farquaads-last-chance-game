db4d2defcf0f2111fa08a024f8951cc844c32f82
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-23 21:57:39 -0400

    completed platforms 7-12

--- main.js
+++ main.js
@@ -85,6 +85,13 @@ let platform_3 = new Object(0, 960, 128, 192);
 let platform_4 = new Object(128, 832, 672, 32);
 let platform_5 = new Object(0, 704, 576, 32);
 let platform_6 = new Object(0, 576, 160, 32);
+let platform_7 = new Object(224, 448, 96, 32);
+let platform_8 = new Object(352, 416, 96, 32);
+let platform_9 = new Object(480, 384, 96, 32);
+let platform_10 = new Object(640, 352, 160, 32);
+let platform_11 = new Object(0, 256, 576, 32);
+let platform_12 = new Object(256, 128, 576, 32);
+
 
 
 
@@ -105,7 +112,14 @@ function animate() {
     platform_4.draw();
     platform_5.draw();
     platform_6.draw();
+    platform_7.draw();
+    platform_8.draw();
+    platform_9.draw();
+    platform_10.draw();
+    platform_11.draw();
+    platform_12.draw();
     
+    // Platform collision detection 
     if (lord.y + lord.height <= platform_1.y && lord.y + lord.height + lord.velocity.y >= platform_1.y && lord.x + lord.width >= platform_1.x && lord.x <= platform_1.x + platform_1.width){
         lord.velocity.y = 0
     }
@@ -122,6 +136,24 @@ function animate() {
         lord.velocity.y = 0
     }
     if (lord.y + lord.height <= platform_6.y && lord.y + lord.height + lord.velocity.y >= platform_6.y && lord.x + lord.width >= platform_6.x && lord.x <= platform_6.x + platform_6.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_7.y && lord.y + lord.height + lord.velocity.y >= platform_7.y && lord.x + lord.width >= platform_7.x && lord.x <= platform_7.x + platform_7.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_8.y && lord.y + lord.height + lord.velocity.y >= platform_8.y && lord.x + lord.width >= platform_8.x && lord.x <= platform_8.x + platform_8.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_9.y && lord.y + lord.height + lord.velocity.y >= platform_9.y && lord.x + lord.width >= platform_9.x && lord.x <= platform_9.x + platform_9.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_10.y && lord.y + lord.height + lord.velocity.y >= platform_10.y && lord.x + lord.width >= platform_10.x && lord.x <= platform_10.x + platform_10.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_11.y && lord.y + lord.height + lord.velocity.y >= platform_11.y && lord.x + lord.width >= platform_11.x && lord.x <= platform_11.x + platform_11.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_12.y && lord.y + lord.height + lord.velocity.y >= platform_12.y && lord.x + lord.width >= platform_12.x && lord.x <= platform_12.x + platform_12.width){
         lord.velocity.y = 0
     }
    
