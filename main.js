7b6b99c3d2c4b55d0f3eb99adc6b8d5da4fb567a
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-05-23 21:06:20 -0400

    set conditionals to detect platform contact

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-window.addEventListener("DOMContentLoaded", function (){
+window.addEventListener("DOMContentLoaded", function (e){
+    const runGame = setInterval(gameLoop, 120)
+
+
+});
 console.log("hello");
 
 const map = document.querySelector('canvas');
@@ -34,7 +38,7 @@ class Crawler {
         this.width = width; 
         this.alive = true;
         this.jumping = false;
-        this.velocity ={
+        this.velocity = {
             x: 0,
             y: 0,
         }
@@ -58,38 +62,69 @@ class Crawler {
 }
 
 
-let lord = new Crawler(596, 96, "red", 32, 32,);
+class Object {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.color = "rgba(0, 0, 255, 0.6)" ;
+        this.width = width;
+        this.height = height;
+       
+    }
+    draw() {
+    
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height,);
+     
+    }
+}
 
+let platform_1 = new Object(224, 1024, 576, 96);
+let platform_2 = new Object(96, 992, 160, 128 )
+
+
+
+let lord = new Crawler(596, 96, "red", 32, 32,);
+let lordFart = new Crawler(300, 96, "blue", 32, 32);
+
+function gameLoop() {
+    ctx.clearRect(0, 0, map.width, map. height)
+}
 
 function animate() {
     requestAnimationFrame(animate)
     ctx.clearRect(0, 0, map.width, map. height)
-    lord.update()
+    lord.update();
+    platform_1.draw();
+    platform_2.draw();
+    
+    if (lord.y + lord.height <= platform_1.y && lord.y + lord.height + lord.velocity.y >= platform_1.y && lord.x + lord.width >= platform_1.x && lord.x <= platform_1.x + platform_1.width){
+        lord.velocity.y = 0
+    }
+    if (lord.y + lord.height <= platform_2.y && lord.y + lord.height + lord.velocity.y >= platform_2.y && lord.x + lord.width >= platform_2.x && lord.x <= platform_2.x + platform_2.width){
+        lord.velocity.y = 0
+    }
+   
 }
 animate();
 
-let shrek = new Crawler(650, 64, "green", 64, 64);
 
 
-
-
-// const runGame = setInterval(gameLoop, 120);
- 
 
 function movementHandler(e){
     console.log("the key that was pressed was: " + e.key);
 
     switch (e.key){
-        case "ArrowLeft":
-            lord.x > 32 ? lord.velocity.x -= 3 : null;
+        case "a":
+            lord.x > 32 ? lord.velocity.x =- 5 : null;
          
             break
-        case "ArrowRight":
-            lord.x < (map.width - 62) ? lord.velocity.x += 3 : null;
+        case "d":
+            lord.x < (map.width - 62) ? lord.velocity.x = 5 : null;
           
             break  
-        case "ArrowUp":
-            lord.velocity.y -= 15
+        case "w":
+            lord.velocity.y =- 15
             break
             
      
@@ -100,13 +135,46 @@ function movementHandler(e){
 
 }
 
+function movementHandlerOff(e){
+    console.log("the key that was pressed was: " + e.key);
+
+    switch (e.key){
+        case "d":
+            lord.velocity.x = 0
+         
+            break
+        case "a":
+            lord.velocity.x = 0
+          
+            break  
+        case "w":
+            lord.velocity.y = 0
+            
+     
+
+        
+    }
+    console.log(lord)
+
+}
+
+// class Platform {
+//     constructor(x, y, width, height){
+//         this.x = x;
+//         this.y = y;
+//         this.width = width;
+//         this.height = height;
+
+//     }
+//     draw(){
+//         ctx.fillStyle = "blue";
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//     }
+// }
 
 
 
-// function gameLoop(){
-//     ctx.clearRect(0, 0, map.width, map. height)
-//     lord.render();
-// };
+
 
 
 
@@ -116,6 +184,5 @@ function movementHandler(e){
 
 
 document.addEventListener("keydown", movementHandler);
-// document.addEventListener("keyup", movementHandler)
+document.addEventListener("keyup", movementHandlerOff)
 
-});
